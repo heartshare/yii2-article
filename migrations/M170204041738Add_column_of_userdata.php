2136ffca7a88e2e4db28154d1716f0bc89c743f7
xuchao <85825770@qq.com>
2017-02-04 12:19:05 +0800

    用户data表增加文章数记录

---
+++ migrations/M170204041738Add_column_of_userdata.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace yuncms\article\migrations;
+
+use yii\db\Migration;
+
+class M170204041738Add_column_of_userdata extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%user_data}}', 'articles', $this->integer()->unsigned()->defaultValue(0)->comment('文章数'));
+    }
+
+    public function down()
+    {
+        $this->dropColumn('{{%user_data}}', 'articles');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
