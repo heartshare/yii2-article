1d93090491050810c80f99ceb8de1618c0d9f17e
xuchao <85825770@qq.com>
2017-02-07 18:16:09 +0800

    AA

--- views/article/_item.php
+++ views/article/_item.php
@@ -4,18 +4,18 @@ use yii\helpers\Html;
 
 ?>
 
-
+<?php if($model->cover):?>
 <div class="media-left">
     <a href="<?= Url::to(['/article/article/view', 'key' => $model->key]); ?>">
         <img class="media-object" src="<?= $model->cover; ?>" alt="utf-8">
     </a>
 </div>
-
+<?php endif;?>
 <div class="media-body">
     <div class="media-heading">
-        <?= Html::encode($model->title) ?>
+        <a href="<?= Url::to(['/article/article/view', 'key' => $model->key]); ?>" target="_blank"><?= Html::encode($model->title) ?></a>
     </div>
-    <div class="media-content"><?= mb_substr(Html::encode($model->data->content), 0, 100) ?></div>
+    <div class="media-content"><?= mb_substr(strip_tags($model->data->content), 0, 100) ?></div>
     <div class="media-action"><?= Yii::$app->formatter->asRelativeTime($model->created_at); ?>
 
     </div>
