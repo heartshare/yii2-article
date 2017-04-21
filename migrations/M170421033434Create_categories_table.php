b435cd6eab51616a025c7255a0e26a93c0817ec7
xuchao <85825770@qq.com>
2017-04-21 11:38:44 +0800

    增加 栏目表

---
+++ migrations/M170421033434Create_categories_table.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace yuncms\article\migrations;
+
+use yii\db\Migration;
+
+class M170421033434Create_categories_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%article_categories}}', [
+            'id' => $this->primaryKey(),
+            'parent' => $this->integer(),
+            'name' => $this->string()->notNull()->comment('名字'),
+            'slug' => $this->string(50),
+            'keywords' => $this->string(),
+            'description' => $this->string(1000)->defaultValue(''),
+            'pinyin' => $this->string(),
+            'letter' => $this->string(1),
+            'frequency' => $this->integer()->notNull()->defaultValue(0),
+            'sort' => $this->smallInteger(5)->notNull()->defaultValue(0),
+            'allow_publish' => $this->boolean()->defaultValue(true)->comment('是否允许发布内容'),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->addForeignKey('{{%article_categories_fk}}', '{{%article_categories}}', 'parent', '{{%article_categories}}', 'id', 'SET NULL', 'CASCADE');
+
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%article_categories}}');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ models/Category.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\article\models;
+
+use Yii;
+use yii\db\Query;
+use yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
+use Overtrue\Pinyin\Pinyin;
+
+/**
+ * Class Category
+ * @property int $id ID
+ * @property integer $parent 父ID
+ * @property string $name 标题
+ * @property string $keywords 关键词
+ * @property string $description 描述
+ * @property string $pinyin 拼音
+ * @property string $letter 首字母
+ * @property int $frequency 热度
+ * @package common\models
+ */
+class Category extends ActiveRecord
+{
+    /**
+     * @var string 父栏目名称
+     */
+    public $parent_name;
+
+    /**
+     * @inheritdoc
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%article_categories}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'slug'], 'required'],
+            [['slug'], 'string', 'max' => 20],
+            [['letter'], 'string', 'max' => 1],
+            [['keywords', 'pinyin'], 'string', 'max' => 255],
+            [['description'], 'string', 'max' => 1000],
+
+            [['parent_name'], 'in',
+                'range' => static::find()->select(['name'])->column(),
+                'message' => 'Category "{value}" not found.'],
+            [['parent', 'sort', 'slug'], 'default'],
+            [['parent'], 'filterParent', 'when' => function () {
+                return !$this->isNewRecord;
+            }],
+            [['sort', 'frequency'], 'integer'],
+            ['sort', 'default', 'value' => 0]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('article', 'ID'),
+            'name' => Yii::t('article', 'Category Name'),
+            'parent' => Yii::t('article', 'Parent Category'),
+            'slug' => Yii::t('article', 'Category Slug'),
+            'keywords' => Yii::t('article', 'Category Keywords'),
+            'description' => Yii::t('article', 'Category Description'),
+            'pinyin' => Yii::t('article', 'Pinyin'),
+            'letter' => Yii::t('article', 'Letter'),
+            'frequency' => Yii::t('article', 'Frequency'),
+            'sort' => Yii::t('article', 'Sort'),
+            'allow_publish' => Yii::t('article', 'Allow Publish'),
+            'parent_name' => Yii::t('article', 'Parent Category'),
+            'created_at' => Yii::t('article', 'Created At'),
+            'updated_at' => Yii::t('article', 'Updated At'),
+        ];
+    }
+
+    /**
+     * 获取父栏目
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategoryParent()
+    {
+        return $this->hasOne(self::className(), ['id' => 'parent']);
+    }
+
+    /**
+     * 获取子栏目
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategories()
+    {
+        return $this->hasMany(static::className(), ['parent' => 'id']);
+    }
+
+    /**
+     * Use to loop detected.
+     */
+    public function filterParent()
+    {
+        $parent = $this->parent;
+        $db = static::getDb();
+        $query = (new Query)->select(['parent'])
+            ->from(static::tableName())
+            ->where('[[id]]=:id');
+        while ($parent) {
+            if ($this->id == $parent) {
+                $this->addError('parent_name', Yii::t('article', 'Loop detected.'));
+                return;
+            }
+            $parent = $query->params([':id' => $parent])->scalar($db);
+        }
+    }
+
+    /** @inheritdoc */
+    public function beforeSave($insert)
+    {
+        if (empty($this->pinyin)) {
+            $py = new Pinyin();
+            $this->pinyin = strtolower($py->permalink($this->name, ''));
+        }
+        if (empty($this->letter)) {
+            $this->letter = strtoupper(substr($this->pinyin, 0, 1));
+        }
+        return parent::beforeSave($insert);
+    }
+}
