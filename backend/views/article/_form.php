ce6f6be286290a30c1e337b7e1ffc4d9f57c090a
xuchao <85825770@qq.com>
2017-04-22 11:14:01 +0800

    update

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -11,39 +11,38 @@ use xutl\inspinia\ActiveForm;
 /* @var ActiveForm $form */
 ?>
 <?php $form = ActiveForm::begin(['layout' => 'horizontal']); ?>
-<fieldset>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'sub_title')->textInput(['maxlength' => true]) ?>
+<?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'sub_title')->textInput(['maxlength' => true]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Category::getDropDownList(), 'id', 'name')); ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'status')->inline(true)->radioList([Article::STATUS_ACTIVE => Yii::t('article', 'Active'), Article::STATUS_PENDING => Yii::t('article', 'Pending')]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'cover')->textInput(['maxlength' => true]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'cover')->fileInput(['style' => 'max-width:200px;max-height:200px']); ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'is_top')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'is_hot')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'is_best')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'description')->textInput(['maxlength' => true, 'rows' => 5]) ?>
+<div class="hr-line-dashed"></div>
+<?= $form->field($model, 'content')->widget(UEditor::className(), [
 
-    <?=$form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Category::getDropDownList(), 'id', 'name')); ?>
+]) ?>
 
-    <?= $form->field($model, 'status')->inline(true)->radioList([Article::STATUS_ACTIVE => Yii::t('article', 'Active'), Article::STATUS_PENDING => Yii::t('article', 'Pending')]) ?>
-
-    <?= $form->field($model, 'cover')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cover')->fileInput(['style' => 'max-width:200px;max-height:200px']); ?>
-
-    <?= $form->field($model, 'is_top')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
-
-    <?= $form->field($model, 'is_hot')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
-
-    <?= $form->field($model, 'is_best')->inline(true)->radioList([true => Yii::t('app', 'Yes'), false => Yii::t('app', 'No')]) ?>
-
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true, 'rows' => 5]) ?>
-
-    <?= $form->field($model, 'content')->widget(UEditor::className(), [
-
-    ]) ?>
-</fieldset>
-<div class="form-actions">
-    <div class="row">
-        <div class="col-md-12">
-            <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
+<div class="form-group">
+    <div class="col-sm-4 col-sm-offset-2">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 </div>
+
 
 <?php ActiveForm::end(); ?>
 
